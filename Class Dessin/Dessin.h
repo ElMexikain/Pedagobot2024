6f75aaee011bb5e41b3875db4812a1b2967eb398
ElMexikain <153193255+ElMexikain@users.noreply.github.com>
2023-12-13 11:21:56 +0100

    Update Dessin.h

--- Class Dessin/Dessin.h
+++ Class Dessin/Dessin.h
@@ -10,6 +10,7 @@ class Dessin(){
         void pavage_heptagone(double rayon_interieur);
         void pavage_triangle(double longueur);
         void pavage_cercle(double rayon);
+        void pavage_triangle(int longueur, int répétitions);
         private: 
 
 };
