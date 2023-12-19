66311deaf1c2ad805eec99aaaf35ea2ff2d0cd9c
ElMexikain <153193255+ElMexikain@users.noreply.github.com>
2023-12-19 15:21:16 +0100

    Update Deplacement.h

--- Class_Deplacement/Deplacement.h
+++ Class_Deplacement/Deplacement.h
@@ -10,6 +10,7 @@ public:
         void reculer(int distance);
         void tourner_gauche(int angle);
         void tourner_droite(int angle);
+        void calculDistance();
         
 private:
 };
