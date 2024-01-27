74135b2d6598a5b11f81c256d2075bb52e17b996
Milkoeee <152439253+Milkoeee@users.noreply.github.com>
2024-01-27 13:36:35 +0100

    Update Codecarre.ino
    Included class deplcement to instatiate robot

--- Codecarre.ino
+++ Codecarre.ino
@@ -2,27 +2,22 @@
 Deplacement carre(0,0);
 unsigned long debut;
 unsigned long fin;
-unsigned long duree;
-const int moteur_droite_pin1 = 7;
-const int moteur_droite_pin2 = 6;
-const int moteur_gauche_pin1 = 8;
-const int moteur_gauche_pin2 = 9;
+unsigned long distance;
+
+Deplacement * robot = new Deplacement();
 void setup() {
-  pinMode(moteur_droite_pin1,OUTPUT);
-  pinMode(moteur_droite_pin2,OUTPUT);
-  pinMode(moteur_gauche_pin1,OUTPUT);
-  pinMode(moteur_gauche_pin2,OUTPUT);
-  int carre.distance = Serial.parseInt();
-  int carre.angle = Serial.parseInt();
-  Serial.begin(9600); 
+  // put your setup code here, to run once:
+  (*robot).init();
+  Serial.begin(9600);
+
 }
 
 void loop() {
-    carre.avancer();
-    carre.tourner();
-    carre.avancer();
-    carre.tourner();
-    carre.avancer();
-    carre.tourner();
-    carre.avancer();
+    robot.avancer(distance);
+    robot.tourner_gauche(90);
+    robot.avancer(distance);
+    robot.tourner_gauche(90);
+    robot.avancer(distance);
+    robot.tourner_gauche(90);
+    robot.avancer(distance);
 }
