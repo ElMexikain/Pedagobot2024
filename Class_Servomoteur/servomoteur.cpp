1c7593290c78bde4a0fe017ab35aff9399b11b0d
ElMexikain <153193255+ElMexikain@users.noreply.github.com>
2024-01-28 19:46:14 +0100

    Update servomoteur.cpp

--- Class_Servomoteur/servomoteur.cpp
+++ Class_Servomoteur/servomoteur.cpp
@@ -6,15 +6,16 @@
 Servo myservo; //créer un objet appelé myservo à partir du module Servo
 
 //declaration des variables
-
-int pos=0; //variable pour stocker la position courante du servo
-int buttonState=0; //variable qui sera utilisée pour stocker l'état du button
-
-//initialisationdes constantes
-const int buttonPin=2; //numéro de la broche à laquelle est connectée le bouton poussoir
-
-
-
+double vitesseCCW=26;//vitesse clock wise
+double vitesseCW=-26;//vitesse counter clockcwise
+double zero=0; // on défini un zéro :)
+double stop=map(zero,-100,100,0,180); on redéfini le zéro sur la plage 0, 180 du servo
+double vitesse_adjCCW=map(vitesseCCW,-100,100,0,180);on redéfini la vitesse CCW sur la plage 0, 180 du servo
+double vitesse_adjCW=map(vitesseCW,-100,100,0,180);on redéfini la vitesse CCW sur la plage 0, 180 du servo
+double angle=180; //angle demandé en degrés
+double coeff= 5.9;//nb secondes par degré pour une vitesse de 26
+double temps = angle*coeff; // temps nécessaire pour tourner de l'angle défini
+int pos=0;//position du servomoteur
 void servomoteur::init()
 {
   ESP32PWM::allocateTimer(0);
@@ -22,76 +23,60 @@ void servomoteur::init()
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   myservo.setPeriodHertz(50);
-  myservo.attach(2,1000,2000);//attaché notre objet myservo au servomoteur branché sur la broche 2
+  myservo.attach(A0,1000,2000);//attaché notre objet myservo au servomoteur branché sur la broche 2
 }
 
 
 void servomoteur::stylo_1()
 {
-  
-  //digitalWrite(ledVertePin,HIGH);//On allume la led Verte
-  //digitalWrite(ledJaunePin,LOW);//On éteind la led Jaune
-  //digitalWrite(ledRougePin,LOW);//On éteind la led Rouge
-  
   if(pos==0){ //Si aucun des stylos ne fonctionnait, on le raméne à la position du stylo 1
-    for(pos=0;pos>-60;pos-=1){
-      myservo.write(pos);
-      delay(15);
-    }
+    myservo.write(vitesse_adjCCW);
+    delay(temps);
+    myservo.write(stop);
+    pos=-angle;
   }
     
-  if(pos==60){ //Si le stylo 2 fonctionnait, on le raméne à la position du stylo 1
-    for(pos=60;pos>-60;pos-=1){
-      myservo.write(pos);
-      delay(15);
-    }  
+  if(pos==angle){ //Si le stylo 2 fonctionnait, on le raméne à la position du stylo 1
+    myservo.write(vitesse_adjCCW);
+    delay(2*temps);
+    myservo.write(stop);
+    pos=-angle;
   }
   
 }
  
 void servomoteur::stylo_2()
-{
-  
-  //digitalWrite(ledRougePin,HIGH);//On allume la led Rouge
-  //digitalWrite(ledJaunePin,LOW);//On éteind la led Jaune
-  //digitalWrite(ledVertePin,LOW);//On éteind la led Verte
-  
+{ 
   if(pos==0){ //Si aucun des stylos ne fonctionnait, on le raméne à la position du stylo 2
-    for(pos=0;pos<60;pos+=1){
-      myservo.write(pos);
-      delay(15);
-    }
+     myservo.write(vitesse_adjCW);
+    delay(temps);
+    myservo.write(stop);
+    pos=angle;
   }
     
-  if(pos==-60){ //Si le stylo 1 fonctionnait, on le raméne à la position du stylo 2
-    for(pos=-60;pos<60;pos+=1){
-      myservo.write(pos);
-      delay(15);
-    }  
+  if(pos==-angle){ //Si le stylo 1 fonctionnait, on le raméne à la position du stylo 2
+    myservo.write(vitesse_adjCW);
+    delay(2*temps);
+    myservo.write(stop);
+    pos=angle;
   }
   
 }
 
 void servomoteur::non_stylo()
 {
-  
-  //digitalWrite(ledJaunePin,HIGH);//On allume la led Jaune
-  //digitalWrite(ledVertePin,LOW);//On éteind la led Verte
-  //digitalWrite(ledRougePin,LOW);//On éteind la led Rouge
-  
-  if(pos==-60){ //Si le stylo 1 fonctionnait, on le remonte
-    for(pos=-60;pos<0;pos+=1){
-      myservo.write(pos);
-      delay(15);
-    }
+  if(pos==-angle){ //Si le stylo 1 fonctionnait, on le remonte
+    myservo.write(vitesse_adjCW);
+    delay(temps);
+    myservo.write(stop);
+    pos=0;
   }
     
-  if(pos==60){ //Si le stylo 2 fonctionnait, on le remonte
-    for(pos=-60;pos>0;pos-=1){
-      myservo.write(pos);
-      delay(15);
-    }  
+  if(pos==angle){ //Si le stylo 2 fonctionnait, on le remonte
+    myservo.write(vitesse_adjCCW);
+    delay(temps);
+    myservo.write(stop);
+    pos=0;
   }
   
 }
-
