d3e847a863e07386436973c31d4b10335906b2b8
ElMexikain <153193255+ElMexikain@users.noreply.github.com>
2024-01-28 19:43:01 +0100

    Update servomoteur.ino

--- Class_Servomoteur/servomoteur.ino
+++ Class_Servomoteur/servomoteur.ino
@@ -4,17 +4,15 @@
 servomoteur test;
 
 void setup() {
- 
-
   test.init();
 
 }
 
 void loop() {
-  
-  test.stylo_1;
+  test.stylo_1();
   delay(5000);
-  test.stylo_2;
+  test.stylo_2();
   delay(5000);
-
+  test.non_stylo();
+  delay(5000);
 }
