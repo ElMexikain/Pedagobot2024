bce17cc0b9c93f1c4f4e2b14815071444a53ea04
ElMexikain <153193255+ElMexikain@users.noreply.github.com>
2024-02-11 21:43:34 +0100

    Update servomoteur.cpp

--- Class_Servomoteur/servomoteur.cpp
+++ Class_Servomoteur/servomoteur.cpp
@@ -25,25 +25,20 @@ void servomoteur::init()
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   myservo.setPeriodHertz(50);
-  myservo.attach(A0,1000,2000);//attaché notre objet myservo au servomoteur branché sur la broche A0
+  myservo.attach(A0,1000,2000); on associe la broche A0 au servomoteur
 }
 
 
 void servomoteur::stylo_1()
 {
-
-  //digitalWrite(ledVertePin,HIGH);//On allume la led Verte
-  //digitalWrite(ledJaunePin,LOW);//On éteind la led Jaune
-  //digitalWrite(ledRougePin,LOW);//On éteind la led Rouge
-
-  if(pos==0){ //Si aucun des stylos ne fonctionnait, on le raméne à la position du stylo 1
+  if(pos==0){ //Si aucun des stylos ne fonctionnait, on le ramène à la position du stylo 1
     for(pos=0;pos>-60;pos-=1){
       myservo.write(pos);
       delay(15);
     }
   }
 
-  if(pos==60){ //Si le stylo 2 fonctionnait, on le raméne à la position du stylo 1
+  if(pos==60){ //Si le stylo 2 fonctionnait, on le ramène à la position du stylo 1
     for(pos=60;pos>-60;pos-=1){
       myservo.write(pos);
       delay(15);
@@ -54,11 +49,6 @@ void servomoteur::stylo_1()
 
 void servomoteur::stylo_2()
 {
-
-  //digitalWrite(ledRougePin,HIGH);//On allume la led Rouge
-  //digitalWrite(ledJaunePin,LOW);//On éteind la led Jaune
-  //digitalWrite(ledVertePin,LOW);//On éteind la led Verte
-
   if(pos==0){ //Si aucun des stylos ne fonctionnait, on le raméne à la position du stylo 2
     for(pos=0;pos<60;pos+=1){
       myservo.write(pos);
@@ -77,11 +67,6 @@ void servomoteur::stylo_2()
 
 void servomoteur::non_stylo()
 {
-
-  //digitalWrite(ledJaunePin,HIGH);//On allume la led Jaune
-  //digitalWrite(ledVertePin,LOW);//On éteind la led Verte
-  //digitalWrite(ledRougePin,LOW);//On éteind la led Rouge
-
   if(pos==-60){ //Si le stylo 1 fonctionnait, on le remonte
     for(pos=-60;pos<0;pos+=1){
       myservo.write(pos);
