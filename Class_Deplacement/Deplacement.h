24499fd0d48c5d3777cee9d24c4be7e7dd5c3143
Milkoeee <152439253+Milkoeee@users.noreply.github.com>
2024-02-28 10:35:10 +0100

    Mise à jour en implémentant les moteurs

--- Class_Deplacement/Deplacement.h
+++ Class_Deplacement/Deplacement.h
@@ -2,17 +2,15 @@
 #define Deplacement_h
 #include "Arduino.h" 
 #include "math.h"
-#include <Stepper.h>
+#include "Moteur.h"
 //#include "ArduinoOTA.h"
 //#include "WiFi.h
 
 class Deplacement{		
 public:
-        void init();
         int distanceToStep(float distance);
         void avancer(float distance);
         void reculer(float distance);
-        void vitesse(int v);
         void tourner_gauche(float angle);
         void tourner_droite(float angle);
         float calculDistance();
