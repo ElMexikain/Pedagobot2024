daae634afbf7ebbec12a997b45827e459902788f
HoussaineA <153200784+HoussaineA@users.noreply.github.com>
2024-04-01 15:23:50 +0200

    Ajout de EnvoiArduino.bat
    Workflow pour l'envoi de commandes Arduino depuis mBlock

---
+++ EnvoiArduino.bat
@@ -0,0 +1,14 @@
+cd %HOMEPATH%\mblock-avr\temp\build
+
+IF exist CodeArduino\ ( cd CodeArduino\ && rm CodeArduino.ino && cd .. && rmdir CodeArduino\ ) 
+findstr /v "Wire.h SoftwareSerial.h" code.cpp > CodeArduino.ino 
+mkdir "CodeArduino" 
+mv CodeArduino.ino CodeArduino/
+cd CodeArduino/
+arduino-cli board list > temp.txt
+type temp.txt | findstr /b /r /c:^COM | findstr Arduino > temp2.txt
+set /p portUSB=<temp2.txt
+arduino-cli compile -b arduino:esp32:nano_nora -v
+arduino-cli upload -b arduino:esp32:nano_nora -p %portUSB:~0,4% -v
+rm temp*
+
